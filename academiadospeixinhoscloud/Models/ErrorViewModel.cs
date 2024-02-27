e7e1cc3c9387229551df7b9aef4e4ec57f8eebda
ricar <ricar@RICARDO-TOWER>
2024-02-27 22:29:48 +0000

    Add project files.

---
+++ academiadospeixinhoscloud/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace academiadospeixinhoscloud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
