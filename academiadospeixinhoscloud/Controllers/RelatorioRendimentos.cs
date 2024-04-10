37bc1e3922557e9a313e06eafad6b4d3f375037d
Ricardo Gonçalves <ricardogoncalves126@gmail.com>
2024-04-10 17:47:05 +0100

    added missing pages

---
+++ academiadospeixinhoscloud/Controllers/RelatorioRendimentos.cs
@@ -0,0 +1,20 @@
+﻿using academiadospeixinhoscloud.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace academiadospeixinhoscloud.Controllers
+{
+    public class RelatorioRendimentos : Controller
+    {
+
+        private readonly academiadospeixinhoscloudContext _context;
+
+        public RelatorioRendimentos(academiadospeixinhoscloudContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ academiadospeixinhoscloud/Controllers/SubscricaosSimulador.cs
@@ -0,0 +1,20 @@
+﻿using academiadospeixinhoscloud.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace academiadospeixinhoscloud.Controllers
+{
+    public class SubscricaosSimulador : Controller
+    {
+
+        private readonly academiadospeixinhoscloudContext _context;
+
+        public SubscricaosSimulador(academiadospeixinhoscloudContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
